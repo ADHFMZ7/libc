46b3b968f86b73a206ddaac3f387750a8617dee3
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-01-17 19:31:53 -0800

    implement allocate_space

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This C library reimplements some of the C standard library functions to help mys
 
 My implementation of malloc uses the linked list method to keep track of data. Each block of data that is 
 allocated has a small struct that stores metadata about the block. This includes the amount of data it 
-holds, a pointer to the next block, and how much free space is adjacent to it.
+holds, a pointer to the next block, and if the block is free or not. 
 
 This gives the malloc function the ability to go through the linked list until a big enough block of free
 space is found. It then allocates the memory and appends itself to the linked list. 
--- myalloc.c
+++ myalloc.c
@@ -1,23 +1,75 @@
+#include <assert.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define HEAP_SIZE 64000 // 
-
-size_t size = sizeof(size_t) + sizeof(void*) + sizeof(int);
+#define HEAP_SIZE 64000 // defines the size of the heap
 
 //The struct that holds meta data of block
-struct metadata
+typedef struct metadata
 {
-	size_t size;
-	struct metadata *next;
-	int free;
-};
+	size_t size;										 // the size of the allocated block
+	struct metadata *next;					 // A pointer to the next block in the linkedlist
+	int free;												 // 
+} metadata;
+
+#define META_SIZE sizeof(metadata) // defines size of metadata block
+
+metadata* head = NULL;						 // head of linked list
+
+// Will call sbrk to allocate desired amount of space and create a new metablock with it.
+metadata *allocate_space(metadata *prev, size_t size)
+{
+	// create a new metadata struct	
+	metadata block ={.next = NULL, .size = size, .free = 0};
+
+	// set prev blocks next to this block
+	prev->next = &block;
+
+	// call sbrk to create block of size + size of metadata
+	void *memory = sbrk(META_SIZE + size)	;
+
+	// catch errors thrown by sbrk syscall
+	assert(memory != (void *) -1 && "sbrk failed to allocate memory.");
+
+	return memory;
+}
+
+metadata *find_free(size_t size)
+{
+
+	return NULL;
+}
 
 
 void *myalloc(size_t size)
 {
+	if (size <= 0)
+	{
+		return NULL;
+	}
+
+	
+	// We want to initialize the linked list if it is empty
+	if (!head)
+	{
+		head = allocate_space(NULL, size);
+
+		return head;
+	}
+	// otherwise, we want to check if there is a free block of the desired size
+	else if (find_free(size) != NULL)
+	{
 
 
-	return NULL;	
+		return NULL;
+	}
+	else {
+		//append new allocated space
+
+		return NULL;
+	}
+
+
 }
 
 
@@ -27,5 +79,3 @@ void myfree(void* location)
 
 
 }
-
-
