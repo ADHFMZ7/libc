34bc8282f935f4359df07ed0538b525e7d5ba7af
Ahmad Aldasouqi <ahmadaldasouqi559@gmail.com>
2023-02-25 21:33:57 -0800

    Update myalloc.h

--- myalloc.h
+++ myalloc.h
@@ -5,9 +5,9 @@
 
 typedef struct metadata
 {
-	size_t size;										 // the size of the allocated block
-	struct metadata *next;					 // A pointer to the next block in the linkedlist
-	int free;												 // 0/1 if block is free or not
+    size_t size;            // the size of the allocated block
+    struct metadata *next;  // A pointer to the next block in the linkedlist
+    int free;	            // 0/1 if block is free or not
 } metadata;
 
 metadata *allocate_space(metadata *prev, size_t size);
